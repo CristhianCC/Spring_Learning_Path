8d73dbfe6df83fbb830f0c9fb415a207dee3ad7e
CristhianCC <cristhianmcm12@gmail.com>
2021-03-11 14:10:21 +0100

    Spring Context instantiated just to see an example of the controller bean.

--- 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/ExampleApplication.java
+++ 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/ExampleApplication.java
@@ -1,13 +1,21 @@
 package com.spring.injection.dependency.example;
 
+import com.spring.injection.dependency.example.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExampleApplication.class, args);
+		ApplicationContext appCtx = SpringApplication.run(ExampleApplication.class, args);
+
+		MyController myController = (MyController) appCtx.getBean("myController");
+
+		String greet = myController.sayHiToEveryone();
+
+		System.out.println(greet);
 	}
 
 }
