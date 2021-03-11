e0eb0903ac3d52175768c224d7ecc4f2c55d73cc
CristhianCC <cristhianmcm12@gmail.com>
2021-03-11 14:40:08 +0100

    Creation of SetterInjectedController

---
+++ 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/controllers/SetterInjectedController.java
@@ -0,0 +1,17 @@
+package com.spring.injection.dependency.example.controllers;
+
+import com.spring.injection.dependency.example.services.GreetingService;
+
+public class SetterInjectedController {
+
+    // This is now a private attribute
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String greet() {
+        return this.greetingService.sayHiToEveryone();
+    }
+}
