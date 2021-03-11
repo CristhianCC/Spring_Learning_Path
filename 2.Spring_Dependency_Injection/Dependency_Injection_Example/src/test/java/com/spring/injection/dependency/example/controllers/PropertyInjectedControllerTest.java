3cd69b3583396960d8f027f237316de0f9d386c3
CristhianCC <cristhianmcm12@gmail.com>
2021-03-11 14:36:06 +0100

    Creation of PropertyInjectedControllerTest to imitate what Spring does when it injects a component.

---
+++ 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/test/java/com/spring/injection/dependency/example/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package com.spring.injection.dependency.example.controllers;
+
+import com.spring.injection.dependency.example.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyInjectedControllerTest {
+
+    PropertyInjectedController propertyInjectedController;
+
+    @BeforeEach
+    void setUp() {
+        this.propertyInjectedController = new PropertyInjectedController();
+        this.propertyInjectedController.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(this.propertyInjectedController.greet());
+    }
+}
