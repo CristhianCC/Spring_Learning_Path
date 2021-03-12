067ad37be447b080dfbddffd34e83ea05c2640fd
CristhianCC <cristhianmcm12@gmail.com>
2021-03-12 20:49:54 +0100

    Test of how to get the beans of all the controllers created.

--- 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/ExampleApplication.java
+++ 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/ExampleApplication.java
@@ -1,6 +1,9 @@
 package com.spring.injection.dependency.example;
 
+import com.spring.injection.dependency.example.controllers.ConstructorInjectedController;
 import com.spring.injection.dependency.example.controllers.MyController;
+import com.spring.injection.dependency.example.controllers.PropertyInjectedController;
+import com.spring.injection.dependency.example.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +19,18 @@ public class ExampleApplication {
 		String greet = myController.sayHiToEveryone();
 
 		System.out.println(greet);
+
+		System.out.println("--------- Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) appCtx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.greet());
+
+		System.out.println("--------- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) appCtx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.greet());
+
+		System.out.println("--------- Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) appCtx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.greet());
 	}
 
 }
