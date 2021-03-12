204277620acbc6b031cb063c5107eb4ff40cada1
CristhianCC <cristhianmcm12@gmail.com>
2021-03-12 21:20:08 +0100

    To resolver the last error of the autowired beans, Spring has the annotation @Qualifier that allows to specify which bean you want to inject due to the bean name.

--- 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/controllers/PropertyInjectedController.java
+++ 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/controllers/PropertyInjectedController.java
@@ -3,11 +3,13 @@ package com.spring.injection.dependency.example.controllers;
 
 import com.spring.injection.dependency.example.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
+    @Qualifier("propertyInjectedGreetingService")
     @Autowired
     public GreetingService greetingService;
 
--- 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/controllers/SetterInjectedController.java
+++ 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.spring.injection.dependency.example.controllers;
 
 import com.spring.injection.dependency.example.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,6 +11,7 @@ public class SetterInjectedController {
     // This is now a private attribute
     private GreetingService greetingService;
 
+    @Qualifier("setterInjectedGreetingService")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
