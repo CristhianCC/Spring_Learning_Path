dad4cc9df1dc832a815fc5fb987c7145e5121cd5
CristhianCC <cristhianmcm12@gmail.com>
2021-03-13 12:38:40 +0100

    Delete of line.

--- 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/controllers/PropertyInjectedController.java
+++ 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/controllers/PropertyInjectedController.java
@@ -1,6 +1,5 @@
 package com.spring.injection.dependency.example.controllers;
 
-
 import com.spring.injection.dependency.example.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
