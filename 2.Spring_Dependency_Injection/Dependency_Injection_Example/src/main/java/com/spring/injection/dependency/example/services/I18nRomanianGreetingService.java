f6a659f4af8b638b33f091bfe34dd13da13411f8
CristhianCC <cristhianmcm12@gmail.com>
2021-03-13 12:36:46 +0100

    In the file application.properties we configure the active profile that we want to use, this will use all the beans with the annotation @Profile. It's Commonly used for differentiate the environments where we work.

--- 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/services/I18nRomanianGreetingService.java
+++ 2.Spring_Dependency_Injection/Dependency_Injection_Example/src/main/java/com/spring/injection/dependency/example/services/I18nRomanianGreetingService.java
@@ -1,7 +1,9 @@
 package com.spring.injection.dependency.example.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+@Profile("RU")
 @Service("i18nService")
 public class I18nRomanianGreetingService implements GreetingService{
     @Override
